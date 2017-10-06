8207716deade1da3ca726e75b5b2669649c515a9
ralya <muhammad.8507@students.amikom.ac.id>
2017-10-06 10:39:35 +0700

    Login with splash screen

--- amikom-app/app/src/main/java/com/ralyaputra/amikom_app/MainActivity.java
+++ amikom-app/app/src/main/java/com/ralyaputra/amikom_app/MainActivity.java
@@ -3,9 +3,15 @@ package com.ralyaputra.amikom_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText1 ,editText2; //Deklarasi object dari class EdiText
+    String text1 ,text2; //Deklarasi object string
     String msg = "--Amikom App : ";
 
     @Override
@@ -50,5 +56,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         Log.d(msg, "The on Destroy() event");
+    }
+
+    public void loginMasuk(View view) {
+        //Method onClick pada Button
+
+        editText1 = (EditText)findViewById(R.id.edittext_username);
+        editText2 = (EditText) findViewById(R.id.edittext_password);
+        text1 = editText1.getText().toString();
+        text2 = editText2.getText().toString();
+
+        //Kondisi jika username dan password benar maka akan menampilkan pesan text toast
+        //Login sukses dan masuk ke activity 2
+        if ((text1.contains("Username"))&&((text2.contains("Password")))) {
+            Toast.makeText(this, "Login Sukses", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+
+        else if ((text1.matches("")||text2.matches("")))
+          /*
+          Atau jika input text 1 dan text 2 kosong
+           */
+        {
+            //Maka akan menampilkan pesan text toast
+            Toast.makeText(this, "Isikan Username dan Password", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else {
+            //jika kedua kondisi diatas tidak memenuhi
+
+            Toast.makeText(this, "Login Gagal /Username Password Salah", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- amikom-app/app/src/main/java/com/ralyaputra/amikom_app/SplashActivity.java
+++ amikom-app/app/src/main/java/com/ralyaputra/amikom_app/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.ralyaputra.amikom_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,25 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
     }
 }
